d77e2824bc2044523f4c052ad537a79fb1cbff56
UI722726 <ahmed.zalila@rwe.com>
2020-07-09 11:37:08 +0200

    Add accounts components

---
+++ WebAdvert.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class SignupModel
+    {
+
+        [Required]
+        [EmailAddress]
+        [Display(Name="Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(8,ErrorMessage = "Password must be at least 6 characters long!")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password doesn't match!")]
+        public string ConfirmPassword { get; set; }
+
+
+        public SignupModel()
+        {
+        }
+
+
+    }
+}
+    
